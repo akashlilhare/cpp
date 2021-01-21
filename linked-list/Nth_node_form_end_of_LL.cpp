ac4e4094615b015d31085172df9aa9a2996dfdf1
akashlilhare <akashlilhare14@gmail.com>
2021-01-21 23:18:29 +0530

    Nth node from end of linked list

---
+++ linked-list/Nth_node_form_end_of_LL.cpp
@@ -0,0 +1,111 @@
+/*
+Nth node from end of linked list
+
+Given a linked list consisting of L nodes and given a number N. The task is to find the Nth node from the end of the linked list.
+
+Example 1:
+
+Input:
+N = 2
+LinkedList: 1->2->3->4->5->6->7->8->9
+Output: 8
+Explanation: In the first example, there
+are 9 nodes in linked list and we need
+to find 2nd node from end. 2nd node
+from end os 8.  
+
+*/
+
+// C program to find n'th Node in linked list
+#include <stdio.h>
+#include <stdlib.h>
+#include<iostream>
+using namespace std;
+
+/* Link list Node */
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+
+/* Function to get the nth node from the last of a linked list*/
+int getNthFromLast(struct Node* head, int n);
+
+
+
+/* Driver program to test above function*/
+int main()
+{
+  int T,i,n,l,k;
+
+    cin>>T;
+
+    while(T--){
+    struct Node *head = NULL,  *tail = NULL;
+
+        cin>>n>>k;
+        int firstdata;
+        cin>>firstdata;
+        head = new Node(firstdata);
+        tail = head;
+        for(i=1;i<n;i++)
+        {
+            cin>>l;
+            tail->next = new Node(l);
+            tail = tail->next;
+        }
+
+    cout<<getNthFromLast(head, k)<<endl;
+    }
+    return 0;
+}// } Driver Code Ends
+
+
+/* struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+/* Should return data of n'th node from the end of linked list.
+*  head: head of the linked list
+*  n: nth node from end to find
+*/
+int count(Node *head){
+    Node *temp = head;
+    int c =0;
+    while(temp){
+        c++;
+        temp= temp->next;
+    }
+    return c;
+}
+int getNthFromLast(Node *head, int n)
+{
+       int c = count(head);
+       int diff = c -n;
+       
+       Node *temp = head;
+       
+       if(diff<0)
+            return -1;
+       
+       while(temp){
+         if(diff ==0){
+             return temp->data;
+         }
+         diff--;
+         temp = temp->next;
+       }
+       return -1;
+}
+
